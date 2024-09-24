82f417a360104771f50fd995be013734c1752556
Palempallenavya <navyareddyp333@gmail.com>
2024-09-24 16:42:29 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Portfolio from './portfolio';
+
+const App = () => {
+    return (
+        <div className="App">
+            <Portfolio />
+        </div>
+    );
+};
+export default App;
